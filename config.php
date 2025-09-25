4e0562ae2b2954914ffdb85ecfdae5f151eca8cc
ilian-marchitto <imarchitto@OSU.PYTHEAS>
2025-09-25 12:05:50 +0200

    Ajout Config.php

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 // Informations de connexion
-$host = "postgresql-fan2jul.alwaysdata.net";    // ou l'adresse IP du serveur PostgreSQL
-$port = "5432";         // port par défaut PostgreSQL
+$host = "mysql-fan2jul.alwaysdata.net";    // ou l'adresse IP du serveur PostgreSQL
+$port = "3306";         // port par défaut PostgreSQL
 $db   = "fan2jul_database";      // nom de ta base de données
 $user = "fan2jul";     // ton utilisateur PostgreSQL
 $pwd  = "B@nd30rg@n1s33";   // mot de passe utilisateur
 
 try {
     // DSN PostgreSQL
-    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
+    $dsn = "mysql:host=$host;port=$port;dbname=$db";
 
     // Création de la connexion PDO
     $connection = new PDO($dsn, $user, $pwd);
@@ -16,9 +16,10 @@ try {
     // Mode d'erreur : exceptions
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    // Codage de caractères.
+    $connection->exec('SET CHARACTER SET utf8');
+
     echo "✅ Connexion réussie à la base $db";
 } catch (PDOException $e) {
     echo "❌ Erreur de connexion : " . $e->getMessage();
 }
-?>
-
