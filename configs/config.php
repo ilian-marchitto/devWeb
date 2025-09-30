50fdd61d40518038cc1ddba46c21a1868758b622
ilian-marchitto <imarchitto@OSU.PYTHEAS>
2025-09-30 16:17:43 +0200

    Fichier test

--- configs/config.php
+++ configs/config.php
@@ -5,22 +5,5 @@ $host = getenv('DB_HOST');
 $port = getenv('DB_PORT');
 $db   = getenv('DB_NAME');
 $user = getenv('DB_USER');
-$pwd  = getenv('DB_PASS');  
-
-try {
-    // DSN PostgreSQL
-    $dsn = "mysql:host=$host;port=$port;dbname=$db";
-
-    // Création de la connexion PDO
-    $connection = new PDO($dsn, $user, $pwd);
-
-    // Mode d'erreur : exceptions
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Codage de caractères.
-    $connection->exec('SET CHARACTER SET utf8');
-
-    echo "✅ Connexion réussie à la base $db";
-} catch (PDOException $e) {
-    echo "❌ Erreur de connexion : " . $e->getMessage();
-}
+$pwd  = getenv('DB_PASS');
+?>
