58e286015bc9b8340a13383cf0f8aabc7380f752
heather <burbeckheather@gmail.com>
2025-10-04 14:11:42 +0200

    Nettoyage du code et ajout de la fonctionalité se deconnecter

--- views/layout/Header.php
+++ views/layout/Header.php
@@ -1,24 +1,27 @@
 <?php
 
+
 require_once LAYOUT_PATH . '/MenuBuilder.php';
 $isLoggedIn = isset($_SESSION['user']);
 class Header extends MenuBuilder {
-    private MenuBuilder $nav;
-    public function __construct() {
+    public function __construct($isLoggedIn) {
+        $this->nav = new MenuBuilder(); //ELements present dans la nav bar
+        $this->buttonHtml = new MenuBuilder(); // Buttons SeConnecter / Deconnecter
 
-        $this->nav = new MenuBuilder();
-
-        // Menu de navigation
         $this->nav->addItemsLink("Communaute", "#communaute");
         $this->nav->addItemsLink("Description", "#description");
         $this->nav->addItemsLink("Albums", "#albums");
         $this->nav->addItemsLink("Actualite", "#Actualite");
 
-        // Logo et icône utilisateur
         $this->addItemPicture(IMAGES_URL . '/fontJul.png', "#", "", "imageFont");
-        $this->addItemPicture(IMAGES_URL . '/iconUser.webp', BASE_URL . '/index.php?page=seConnecter', "", "userIcon");
-    }
+        if ($isLoggedIn) {
+            $this -> buttonHtml ->addItemsLink("Se déconnecter", BASE_URL . '/index.php?page=logout');
+        } else {
+            $this -> buttonHtml ->addItemsLink("Se connecter", BASE_URL . '/index.php?page=seConnecter');
+        }
 
+
+    }
     public function render() {
         echo "<header>";
 
@@ -31,12 +34,23 @@ class Header extends MenuBuilder {
         }
         echo "</ul></nav>";
 
-        // Logo et icône utilisateur
+        // Logo et autres items
         foreach ($this->items as $item) {
             $this->showOnce($item);
         }
 
+        // Dropdown utilisateur
+        echo '<div class="user-dropdown">';
+        echo '<input type="checkbox" id="toggleUser" class="toggle-checkbox">';
+        echo '<label for="toggleUser">';
+        echo '<img src="' . IMAGES_URL . '/iconUser.webp" alt="Image de connexion" class="login-image">';
+        echo '</label>';
+        echo '<div class="dropdown-content" id="dropdownContent">';
+        foreach ($this->buttonHtml->items as $item) {
+            $this->buttonHtml->showOnce($item);
+        }
+        echo '</div></div>';
+
         echo "</header>";
     }
 }
-?>
