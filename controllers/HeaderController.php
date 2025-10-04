5a05f5a78a615bcf28abe41854ac07f4e75ba09e
heather <burbeckheather@gmail.com>
2025-10-04 14:09:33 +0200

    modification des controllers pour inclure la page se deconnecter et nettoyage du code

--- controllers/HeaderController.php
+++ controllers/HeaderController.php
@@ -1,7 +1,14 @@
 <?php
 require_once LAYOUT_PATH . '/Header.php';
+session_start();
+
+$isLoggedIn = isset($_SESSION['email']);
+
 
 // Créer l’instance de Header
-$header = new Header();
+$header = new Header($isLoggedIn);
 $header->render();
+
+
+
 
--- controllers/accueilController.php
+++ controllers/accueilController.php
@@ -1,6 +1,4 @@
 <?php
-session_start();
-
 $pageTitle = "Bienvenue sur le site";
 
 
