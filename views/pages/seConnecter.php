b1c17429255a9ec562a42a27a3ab94819a872fd6
Ilian <ilian.marchitto@etu.univ-amu.fr>
2025-10-05 23:01:21 +0200

    Création de différents modèles

--- views/pages/seConnecter.php
+++ views/pages/seConnecter.php
@@ -17,14 +17,11 @@
         <div class="login-form">
             <form action="?page=login" method="post" class="login-form">
                 <h2>Connexion</h2>
-                <?php
-                session_start();
-                if (!empty($_SESSION['erreur'])) : ?>
-                    <p class="error" role="alert" aria-live="assertive"><?= htmlspecialchars($_SESSION['erreur']) ?></p>
-                <?php
-                    unset($_SESSION['erreur']);
-                endif;
-                ?>
+                <?php if (!empty($_SESSION['erreur'])) : ?>
+                    <p class="error"><?= htmlspecialchars($_SESSION['erreur']) ?></p>
+                    <?php unset($_SESSION['erreur']); ?>
+                <?php endif; ?>
+
                 <label for="identifiant">Email</label>
                 <input type="email" name="identifiant" id="identifiant" placeholder="juldetp@d&p.com" required>
 
@@ -32,12 +29,12 @@
                 <input type="password" name="mot_de_passe" id="mot_de_passe" placeholder="LeJC'estle100!!" required>
 
                 <button type="submit">Se connecter</button>
-                <p class="forgot">
-                    <a href="<?= BASE_URL ?>/index.php?page=home">Retour à l'accueil</a>
-                    <a href=<?= BASE_URL ?>/index.php?page=forgot>Mot de passe oublié ?</a>
-                </p>
-                <p>Pas encore de compte ? <a href="<?= BASE_URL ?>/index.php?page=sInscrire">Crée le maintenant</a></p>
             </form>
+            <p class="forgot">
+                <a href="<?= BASE_URL ?>/index.php?page=home">Retour à l'accueil</a>
+                <a href=<?= BASE_URL ?>/index.php?page=forgot>Mot de passe oublié ?</a>
+            </p>
+            <p>Pas encore de compte ? <a href="<?= BASE_URL ?>/index.php?page=sInscrire">Crée le maintenant</a></p>
         </div>
     </div>
 </body>
