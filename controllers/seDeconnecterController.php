0fd9e462343b6a8ea4e18fee2ef865931f335d72
Sofia Ach <sofia.ach@etu.univ-amu.fr>
2025-10-08 17:42:41 +0200

    modification theme page deconnecter

--- controllers/seDeconnecterController.php
+++ controllers/seDeconnecterController.php
@@ -1,6 +1,32 @@
 <?php
 // Démarrer la session
 session_start();
+require_once CONTROLLERS_PATH . '/HeadController.php';
+require_once CONTROLLERS_PATH .'/toggleButtonController.php';
+
+toggleButtonController::handleThemeToggle();
+$styleDynamique = toggleButtonController::getActiveStyle();
+
+
+// ==========================
+// Variables spécifiques à la page
+// ==========================
+
+
+$pageTitle = "Se connecter";
+$pageDescription = "Site officiel des auteurs ACH Sofia, ARFI Maxime, BURBECK Heather et MARCHITTO Ilian. Connectez-vous pour découvrir la communauté, les albums et l'actualité.";
+$pageKeywords = "Fan2Jul, ACH Sofia, ARFI Maxime, BURBECK Heather, MARCHITTO Ilian, communauté, connexion";
+$pageAuthor = "ACH Sofia, ARFI Maxime, BURBECK Heather, MARCHITTO Ilian";
+$pageCss = ["seConnecter.css", $styleDynamique]; // Fichier CSS spécifique à la page
+
+// ==========================
+// Contrôleur Head
+// ==========================
+$head = new HeadController($pageTitle, $pageDescription, $pageKeywords, $pageAuthor, $pageCss);
+
+require_once LAYOUT_PATH . '/head.php';
+require_once PAGES_PATH . '/seConnecter.php';
+
 $initiale = '';
 
 
