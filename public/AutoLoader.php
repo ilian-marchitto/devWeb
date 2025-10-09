274a5a1ef1d996f40fa69d618dd72fff0f14cb2d
Ilian <ilian.marchitto@etu.univ-amu.fr>
2025-10-09 23:40:46 +0200

    Petite modification

--- public/AutoLoader.php
+++ public/AutoLoader.php
@@ -4,15 +4,16 @@ class Autoloader
     public static function register()
     {
         spl_autoload_register(function ($class) {
-
+            // Remplacer les backslashes des namespaces par des séparateurs de dossier
             $classPath = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-            // Dossiers où chercher
+
+            // Dossiers racine à explorer
             $paths = [BASE_PATH];
 
             foreach ($paths as $path) {
-                $file = $path . DIRECTORY_SEPARATOR . $class . '.php';
+                $file = $path . DIRECTORY_SEPARATOR . $classPath;
                 if (file_exists($file)) {
-                    require $file;
+                    require_once $file;
                     return true;
                 }
             }
