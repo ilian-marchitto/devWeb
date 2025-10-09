ca83b8e6f4595ab98ebb3a31e6277c4165478d17
Sofia Ach <sofia.ach@etu.univ-amu.fr>
2025-10-09 23:57:25 +0200

    test

--- public/index.php
+++ public/index.php
@@ -53,6 +53,7 @@ $page = filter_input(INPUT_GET, 'page') ?? 'home';
 switch ($page) {
 
     case 'home':
+        if (!file_exists($file)) { var_dump($file); exit('Fichier introuvable'); }
         $controller = new AccueilController($connection);
         $controller->render();
         break;
